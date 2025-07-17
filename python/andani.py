22a5e80b0a7ca6a5ff8fd987872bf0fb0e1d1198
asivhanzhi <mutavhatsindiasivhanzhi@gmail.com>
2025-07-17 12:29:30 +0200

    commit

---
+++ python/andani.py
@@ -0,0 +1,11 @@
+#Loop Control Statements
+
+fruits = ["apple", "banana", "cherry", "date"]
+
+count = 0
+
+while count < 5:
+    print(count)
+    count += 1
+    if count == 3:
+        break # Exist the loop when the count is reached - 3
---
+++ python/rectangle_calc/calculate.py
@@ -0,0 +1,8 @@
+
+def area(length, width):
+    #Function to calculate the area
+    return length * width
+
+def perimeter(lenght, width):
+    #Function to calculate the perimeter
+    return 2 *(lenght + width)
